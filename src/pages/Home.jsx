dbccc28efeb2e653c350dab3fb726cf73bb2e270
Kuol Magok <heiskuol001@gmail.com>
2026-07-30 00:39:36 +0300

    feat: initialize project with React, Vite, and Tailwind CSS
    - Added main application structure with routing (Home and About pages)
    - Integrated Tailwind CSS for styling
    - Included SVG icons for various functionalities
    - Set up Vite configuration for React and Tailwind CSS
    - Created assets for hero image and logos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
