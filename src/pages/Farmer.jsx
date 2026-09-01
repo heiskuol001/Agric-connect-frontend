f0f62ff1deccc3c185b44aea7f555dfd6075ef0c
Kuol Magok <heiskuol001@gmail.com>
2026-09-02 01:22:54 +0300

    feat: add Farmer component to display the FarmerDashboard

---
+++ src/pages/Farmer.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import FarmerDashboard from "../components/farmerComponents/FarmerDashboard"
+
+const Farmer = () => {
+  return (
+    <div>
+      <FarmerDashboard />
+    </div>
+  )
+}
+
+export default Farmer
