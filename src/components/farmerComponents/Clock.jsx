a2a0797ba8dece5fbf3dd4a67275099d1912f790
Kuol Magok <heiskuol001@gmail.com>
2026-09-02 01:20:07 +0300

    refactor: improve formatting and time display in Clock component

--- src/components/farmerComponents/Clock.jsx
+++ src/components/farmerComponents/Clock.jsx
@@ -1,19 +1,24 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const Clock = () => {
-    const [time, setTime] = useState(new Date())
+  const [time, setTime] = useState(new Date());
 
-    useEffect(()=>{
-        const interval = setInterval(() => {
-            setTime(new Date())
-        }, 1000);
-        return ()=> clearInterval(interval)
-    },[])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      {time.toLocaleString()}
+      {time.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Clock
+export default Clock;
