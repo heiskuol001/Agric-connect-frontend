94136771d3c50b4d8f769b32397174594478225f
Kuol Magok <heiskuol001@gmail.com>
2026-09-03 00:38:53 +0300

    feat: add photo utility module with various image imports for enhanced visual assets

---
+++ src/utils/photo.js
@@ -0,0 +1,54 @@
+import opportunities from '../utils/photos/opportunities.jpg'
+import insight from '../utils/photos/insight.jpg'
+import insight1 from '../utils/photos/insight1.jpg'
+import product from '../utils/photos/product.jpg'
+import networking from '../utils/photos/networking.jpg'
+import tractor from '../utils/photos/tractor.jpg'
+import expert1 from '../utils/experts/expert1.jpg'
+import expert2 from '../utils/experts/expert2.jpg'
+import expert3 from '../utils/experts/expert3.jpg'
+import corn from '../utils/photos/corn.jpg'
+import h1 from '../utils/photos/h1.jpg'
+import h2 from '../utils/photos/h2.jpg'
+import order from '../utils/photos/order.jpg'
+import aes from '../utils/photos/aes.jpg'
+import aes1 from '../utils/photos/aes1.jpg'
+import aes2 from '../utils/photos/aes2.jpg'
+import package1 from '../utils/photos/package.png'
+import logOutIcon from '../utils/photos/logouticon.png'
+import messageIcon from '../utils/photos/message.jpg'
+import cartIcon from '../utils/photos/cart.jpg'
+import homeIcon from '../utils/photos/homeIcon.jpg'
+import orderIcon from '../utils/photos/orderIcon.jpg'
+import orderIcon1 from '../utils/photos/order.jpg'
+
+const pics = [
+    opportunities,
+    insight,
+    insight1,
+    product,
+    networking,
+    tractor,
+    expert1,
+    expert2,
+    expert3,
+    corn,
+    h1,
+    h2,
+    order,
+    aes,
+    aes1,
+    aes2,
+    package1,
+    logOutIcon,
+    messageIcon,
+    cartIcon,
+    homeIcon,
+    orderIcon,
+    orderIcon1
+]
+
+
+
+
+export default pics
